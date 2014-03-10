993bf4a4c1516ab8dfcde41f7c03a8e9a6ab693f
xnko <artak.khnkoyan@gmail.com>
2014-03-10 13:04:44 +0400

    initial

--- src/db/src/odbc/db_odbc.h
+++
@@ -1,6 +0,0 @@
-#ifndef DB_ODBC_H_INCLUDED
-#define DB_ODBC_H_INCLUDED
-
-#include "../db_common.h"
-
-#endif // DB_ODBC_H_INCLUDED
--- src/db/src/sqlite3/db_sqlite3.h
+++
@@ -1,6 +0,0 @@
-#ifndef DB_SQLTE3_H_INCLUDED
-#define DB_SQLTE3_H_INCLUDED
-
-#include "../db_common.h"
-
-#endif // DB_SQLTE3_H_INCLUDED
